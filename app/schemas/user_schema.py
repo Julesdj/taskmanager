061d7c876c96e0cf64230777b874b684f17c2b9d
Julesdj <julesdjog@gmail.com>
2025-05-18 19:47:30 -0400

    Feat: Add user schemas

---
+++ app/schemas/user_schema.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class UserBaseSchema(BaseModel):
+    email: str = Field(..., max_length=255)
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class CreateUserSchema(UserBaseSchema):
+    hashed_password: str = Field(..., max_length=255)
+
+
+class UserResponseSchema(UserBaseSchema):
+    id: UUID
+    is_active: bool
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
