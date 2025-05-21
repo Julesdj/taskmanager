9522e570d5a00a5a630d0f1e1749a0821ba9a61d
Julesdj <julesdjog@gmail.com>
2025-05-20 22:08:51 -0400

    Feat: Prevent duplicate emmail registration

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.user import User
@@ -14,3 +15,8 @@ class UserRepository:
         await self.db.commit()
         await self.db.refresh(user)
         return user
+
+    async def get_by_email(self, email: str) -> User | None:
+        result = await self.db.execute(select(User).where(User.email == email))
+
+        return result.scalars().first()
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,10 +1,9 @@
-from passlib.context import CryptContext
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.security import hash_password
 from app.repositories.user_repository import UserRepository
 from app.schemas.user_schema import CreateUserSchema
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class UserService:
@@ -12,8 +11,16 @@ class UserService:
         self.db = db
 
     async def register_user(self, user_in: CreateUserSchema):
-        hashed_pwd = pwd_context.hash(user_in.hashed_password)
+        repository = UserRepository(self.db)
+        existing_user = await repository.get_by_email(user_in.email)
+
+        if existing_user:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Email already registered.",
+            )
+
+        hashed_pwd = hash_password(user_in.hashed_password)
         user_data = user_in.model_copy(update={"hashed_password": hashed_pwd})
 
-        repository = UserRepository(self.db)
         return await repository.create_user(user_data)
