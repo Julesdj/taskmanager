20da9169cb17fb0c6159901cdb006b0c83bdf9e0
Julesdj <julesdjog@gmail.com>
2025-05-22 13:14:09 -0400

    Feat: Add get current user

--- app/api/users.py
+++ app/api/users.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.db.deps import get_db
+from app.db.deps import get_current_user, get_db
+from app.models.user import User
 from app.schemas.user_schema import CreateUserSchema, UserResponseSchema
 from app.services.user_service import UserService
 
@@ -15,3 +16,8 @@ async def register_user(user_in: CreateUserSchema, db: AsyncSession = Depends(ge
     service = UserService(db)
     user = await service.register_user(user_in)
     return user
+
+
+@router.get("/users/me", response_model=UserResponseSchema)
+async def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
